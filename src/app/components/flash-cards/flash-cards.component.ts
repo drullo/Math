81fa267d795590b1df55593aad34dcda5aba268c
Dan Rullo <drullo@cleavelandprice.com>
2018-08-14 15:30:49 -0400

    Added question time toast messages

--- src/app/components/flash-cards/flash-cards.component.ts
+++ src/app/components/flash-cards/flash-cards.component.ts
@@ -2,6 +2,7 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { MatDialog } from '@angular/material/dialog';
+import * as moment from 'moment';
 import { ToastrService } from 'ngx-toastr';
 import { ConfigService } from '@services/config.service';
 import { ScoreDialogComponent } from '@components/score-dialog/score-dialog.component';
@@ -16,11 +17,32 @@ import { FlashCardConfig } from '@model/flash-card-config';
 export class FlashCardsComponent {
   //#region Fields
   currentQuestion = 0; // -1;
+  questionStart = moment();
   //#endregion
 
   //#region Properties
   get config(): FlashCardConfig {
     return this.configService.config;
+  }
+
+  get questionText(): string {
+    const question = this.configService.questions[this.currentQuestion];
+
+    switch (question.operation) {
+      case '+': return `${question.operands[0]} + ${question.operands[1]} = ${question.answer}`;
+      case '-': return `${question.operands[0]} - ${question.operands[1]} = ${question.answer}`;
+      case 'x': return `${question.operands[0]} * ${question.operands[1]} = ${question.answer}`;
+      case '/': return `${question.operands[0]} ÷ ${question.operands[1]} = ${question.answer}`;
+      case '²': return `${question.operands[0]}² = ${question.answer}`;
+      case '³': return `${question.operands[0]}³ = ${question.answer}`;
+      case '√': return `√${question.operands[0]} = ${question.answer}`;
+      case '³√': return `√³${question.operands[0]} = ${question.answer}`;
+    }
+  }
+
+  get invalidInput(): boolean {
+    const currentAnswer = this.configService.questions[this.currentQuestion].answer;
+    return currentAnswer === null || currentAnswer === undefined || +currentAnswer === NaN;
   }
   //#endregion
 
@@ -45,12 +67,14 @@ export class FlashCardsComponent {
   }
 
   nextQuestion(answerInput): void {
+    this.toastQuestionSeconds();
     this.scoreQuestion();
     this.currentQuestion++;
     answerInput.focus();
   }
 
   finish(): void {
+    this.toastQuestionSeconds();
     this.scoreQuestion();
     this.currentQuestion = -1;
     this.dialog.open(ScoreDialogComponent, { disableClose: true })
@@ -60,43 +84,40 @@ export class FlashCardsComponent {
   //#endregion
 
   //#region Utilities
+  private toastQuestionSeconds(): void {
+    const questionEnd = moment();
+    const seconds = questionEnd.diff(this.questionStart, 'seconds');
+    this.toastr.info(`${seconds} seconds: ${this.questionText}`, null, { positionClass: 'toast-top-center' });
+  }
+
   private scoreQuestion(recordScore = true): boolean {
     const question = this.configService.questions[this.currentQuestion];
-    let toast = '';
     let correct = false;
 
     switch (question.operation) {
       case '+':
         correct = question.answer === question.operands[0] + question.operands[1];
-        toast = `${question.operands[0]} + ${question.operands[1]} = ${question.answer}`;
         break;
       case '-':
         correct = question.answer === question.operands[0] - question.operands[1];
-        toast = `${question.operands[0]} - ${question.operands[1]} = ${question.answer}`;
         break;
       case 'x':
         correct = question.answer === question.operands[0] * question.operands[1];
-        toast = `${question.operands[0]} * ${question.operands[1]} = ${question.answer}`;
         break;
       case '/':
         correct = question.answer === question.operands[0] / question.operands[1];
-        toast = `${question.operands[0]} ÷ ${question.operands[1]} = ${question.answer}`;
         break;
       case '²':
         correct = question.answer === Math.pow(question.operands[0], 2);
-        toast = `${question.operands[0]}² = ${question.answer}`;
         break;
       case '³':
         correct = question.answer === Math.pow(question.operands[0], 3);
-        toast = `${question.operands[0]}³ = ${question.answer}`;
         break;
       case '√':
         correct = question.answer === Math.sqrt(question.operands[0]);
-        toast = `√${question.operands[0]} = ${question.answer}`;
         break;
       case '³√':
         correct = question.answer === Math.cbrt(question.operands[0]);
-        toast = `√³${question.operands[0]} = ${question.answer}`;
         break;
     }
 
@@ -105,12 +126,14 @@ export class FlashCardsComponent {
 
       if (this.config.toastMessages) {
         if (question.correctAnswer) {
-          this.toastr.success(toast, 'Correct!', { positionClass: 'toast-top-left' });
+          this.toastr.success(this.questionText, 'Correct!', { positionClass: 'toast-top-left' });
         } else {
-          this.toastr.error(toast, 'Incorrect', { positionClass: 'toast-top-right' });
+          this.toastr.error(this.questionText, 'Incorrect', { positionClass: 'toast-top-right' });
         }
       }
     }
+
+    this.questionStart = moment();
 
     return correct;
   }
